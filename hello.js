b54fd5536a8dc34c7091ea47a96bb65d8f5a1dbc
Sri3009 <32916190+Sri3009@users.noreply.github.com>
2018-02-17 09:40:24 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello\n');
+  res.end('Hello world \n');
 }).listen(8080, '10.0.0.111');
 console.log('Server running at http://10.0.0.111:8080/');
